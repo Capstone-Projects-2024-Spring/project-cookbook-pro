ece8070ecda10a6319b2b15465fb7a5b74a059f8
sdutill <shawndutill@gmail.com>
2024-03-18 11:48:42 -0400

    Example of how to use subscribeToDocument

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
 import "../css/styles.css";
+import FirestoreListener from "../firebase/FirestoreListener.js";
 
 const Header = () => {
   const navigate = useNavigate();
   const { user, logoutUser } = useAuth();
+  const [firestoreListener, setFirestoreListener] = useState(null);
+  const [documentData, setDocumentData] = useState(null);
+
+  useEffect(() => {
+    setFirestoreListener(new FirestoreListener());
+  }, []);
+
+  useEffect(() => {
+    if (user && firestoreListener) {
+      const documentPath = `Users/${user.uid}/SavedRecipes/654959`;
+      const unsubscribe = firestoreListener.subscribeToDocument(
+        documentPath,
+        (snapshot) => {
+          setDocumentData(snapshot.data());
+        }
+      );
+
+      // Cleanup function
+      return unsubscribe;
+    }
+  }, [user, firestoreListener]);
 
   const logoutClick = () => {
     navigate("/login");
+  };
+
+  const handleLogData = () => {
+    if (documentData) {
+      console.log(documentData);
+    }
   };
 
   return (
@@ -27,6 +55,9 @@ const Header = () => {
             <Link to="/search" className="header--link">
               Search
             </Link>
+            <button onClick={handleLogData} className="btn">
+              Log Document Data
+            </button>
             <button onClick={logoutUser} className="btn">
               Logout {user.displayName ? `(${user.displayName})` : ""}
             </button>
