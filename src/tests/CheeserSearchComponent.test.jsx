c06581b83616233c15a6acd411a018cfd2a6b009
sdutill <shawndutill@gmail.com>
2024-04-23 19:28:03 -0400

    Another test

--- src/tests/CheeserSearchComponent.test.jsx
+++ src/tests/CheeserSearchComponent.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import CheeserSearchComponent from "./CheeserSearchComponent";
+import CheeserSearchComponent from "./CheeserSearchComponent.test";
 import ingredientsMap from "../../../customObjects/IngredientMap";
 
 describe("CheeserSearchComponent", () => {
